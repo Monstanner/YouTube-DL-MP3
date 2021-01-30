24d521e93b53badde29ac1ada972851a72115cec
Monstanner <45621250+Monstanner@users.noreply.github.com>
2021-01-30 14:12:40 +0100

    Update YouTube-DL-MP3.bat

--- Windows/YouTube-DL-MP3.bat
+++ Windows/YouTube-DL-MP3.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 SET /P URL="[Fuege den Link zum Video ein. (Rechtsklick > Einfuegen > Enter)] "
 ECHO.
-youtube-dl -xo "#Bitte das ReadMe lesen./%%(titel)s.%%(ext)s" --audio-format mp3 --audio-quality 0 -i --ignore-config --hls-prefer-native %URL%
+youtube-dl -xo "#Bitte das ReadMe.txt lesen./%%(titel)s.%%(ext)s" --audio-format mp3 --audio-quality 0 -i --ignore-config --hls-prefer-native %URL%
 ECHO.
 PAUSE
 EXIT
