8ce5d8541a5ad4d20485435da5ce1498ece2a81b
Monstanner <45621250+Monstanner@users.noreply.github.com>
2021-09-19 01:22:42 +0000

    Update YouTube-DL-MP3.bat
    YouTube-DL MP3 v. 1.2:
    
    Sonstiges:
    
    - Kopzeile Hinzugefügt

--- Windows/YouTube-DL-MP3.bat
+++ Windows/YouTube-DL-MP3.bat
@@ -1,4 +1,7 @@
 @ECHO OFF
+ECHO ---------------------------------------------------
+ECHO YouTube-DL MP3 v.1.2 20210919 by Monstanner
+ECHO ---------------------------------------------------
 SET /P URL="[Fuege den Link zu einem Lied ein. (Rechtsklick > Einfuegen > Enter)] "
 ECHO.
 youtube-dl -xo "#Bitte die ReadMe.txt lesen./%%(titel)s.%%(ext)s" --audio-format mp3 --audio-quality 0 -i --ignore-config --hls-prefer-native %URL%
