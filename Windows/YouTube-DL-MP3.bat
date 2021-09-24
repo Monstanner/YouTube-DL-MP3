5967765571e8868d2ff6a0cc431bcf9b6c774c79
Monstanner <45621250+Monstanner@users.noreply.github.com>
2021-09-24 17:32:29 +0000

    Update YouTube-DL-MP3.bat
    YouTube-DL MP3 v. 1.2.1:
    
    Fehlerbehebungen:
    
    - Der Name des Liedes wurde nicht aus dem Video-Tiel der Webseite importtiert, da ein Schreibfehler bei "(title)s" war. ( Vorher: "(titel)s" Nacher: "(title)s")

--- Windows/YouTube-DL-MP3.bat
+++ Windows/YouTube-DL-MP3.bat
@@ -4,7 +4,7 @@ ECHO YouTube-DL MP3 v.1.2 20210919 by Monstanner
 ECHO ---------------------------------------------------
 SET /P URL="[Fuege den Link zu einem Lied ein. (Rechtsklick > Einfuegen > Enter)] "
 ECHO.
-youtube-dl -xo "#Bitte die ReadMe.txt lesen./%%(titel)s.%%(ext)s" --audio-format mp3 --audio-quality 0 -i --ignore-config --hls-prefer-native %URL%
+youtube-dl -xo "#Bitte die ReadMe.txt lesen./%%(title)s.%%(ext)s" --audio-format mp3 --audio-quality 0 -i --ignore-config --hls-prefer-native %URL%
 ECHO.
 PAUSE
 EXIT
